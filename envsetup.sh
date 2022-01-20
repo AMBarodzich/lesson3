f85bbcba57f47fa6b29046dd32dbcaa8e0a7e8eb
AMBarodzich <sasha-brdich@mail.ru>
2022-01-20 20:43:30 +0300

    dsd
    sad

--- envsetup.sh
+++ envsetup.sh
@@ -4,10 +4,10 @@ if [ -d "env" ]
 then
     echo "Python virtual environment exists." 
 else
-    python3 -m venv env
+    python3 -m virtualenv env
 fi
 
-source bin/activate
+source env/bin/activate
 
 
 pip3 install -r requirements.txt
